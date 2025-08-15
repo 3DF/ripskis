b5a492704ec33cdee98ab4210cd85164623d4d79
3DF <joehallett3@gmail.com>
2025-08-15 09:48:55 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,8 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-use Cowsayphp\Farm;
-
 //header('Content-Type: text/plain');
 header('Content-Type: text/html');
-
-
-$text = "Hi!  I'm the Ripskis cow.  This is the future home of Ripskis.  The best bonghits from around the universe!";
-//$text = "Set a message by adding ?message=<message here> to the URL";
-
-if(isset($_GET['message']) && $_GET['message'] != '') {
-	$text = htmlspecialchars($_GET['message']);
-}
-
-$cow = Farm::create(\Cowsayphp\Farm\Cow::class);
-//echo $cow->say($text);
 
 //$videoPath = "https://ybcledejiqybeyogdgyr.supabase.co/storage/v1/object/public/videos/bong.mp4"; 
 $videoPath = "https://ybcledejiqybeyogdgyr.supabase.co/storage/v1/object/public/videos/IMG_4635.mov";

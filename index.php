84394d4c15a90c59eebf4db954844d5f21db18ce
3DF <joehallett3@gmail.com>
2025-08-15 19:56:38 -0400

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $videoPath = "https://ybcledejiqybeyogdgyr.supabase.co/storage/v1/object/public/
 	<h1>Ripskis</h1>
 	<h2>The best bong hits from around the universe</h2>
 	<br/>
-	<video controls>
+	<video id="videoPlayer" controls>
 		<source src="<?php echo $videoPath; ?>" type="video/mp4">
 		Your browser does not support the video tag.
 	</video>
